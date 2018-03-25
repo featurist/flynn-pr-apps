04d315e8cfd11280d7beb764b4a9b3ef4fa4fa85
artemave <artemave@gmail.com>
2018-03-25 20:55:09 +0100

    Disable TLS certificate validation
    Because flynn is using self signed certificates.

--- features/support/startTestApp.js
+++ features/support/startTestApp.js
@@ -3,9 +3,6 @@ const express = require('express')
 const fs = require('fs-extra')
 const subdomain = require('express-subdomain')
 
-// so that http client can request `https://` urls served by fake flynn api
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
 module.exports = function ({prAppsApp, fakeFlynnApi, port}) {
   const app = express()
 
